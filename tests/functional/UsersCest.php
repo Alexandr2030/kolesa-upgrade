8791b834ac36d6eacb56e9d2eb3a57281ab712ef
Радько Александр <braa@technodom.kz>
2021-06-10 16:09:57 +0600

    Add home work for lesson_09

--- tests/functional/UsersCest.php
+++ tests/functional/UsersCest.php
@@ -4,15 +4,40 @@ use Codeception\Example;
 use Faker\Factory;
 
 /**
+ * Класс для работы с API
+ */
+class API
+{
+    /**
+     * Урла POST запроса
+     */
+    public static $POST = 'human';
+
+    /**
+     * Урла GET запроса
+     */
+    public static $GET = 'people';
+
+    /**
+     * Метод, применимый в PUT и DELETE методах по _id
+     */
+    public static $VARIOUS = 'human?_id=';
+}
+
+
+/**
  * Класс для работы с юзером
  */
 class UsersCest
 {
+    
     /**
-     * Тест на создание юзера
-     * @group test_rest
+     * Тест на создание, изменение и удаление юзера
+     * @group test_user
+     * @dataProvider dataForcheckUserCreate
+     * @param Example $data
      */
-    public function checkUserCreate(\FunctionalTester $I)
+    public function checkUserCreate(\FunctionalTester $I, Example $data)
     {
         $faker = Factory::create();
 
@@ -27,56 +52,41 @@ class UsersCest
 
         $userData = [
             'email' => $faker->email,
-            'owner' => $faker->lastName . '@alex_radko',
+            'owner' => $faker->name.'@alex_radko',
             'job'   => $faker->company,
             'name'  => $faker->firstName
         ];
 
         $I->haveHttpHeader('Content-Type', 'application/json');
-        $I->sendPost('human', $userData);
+        $I->sendPost(API::$POST, $userData);
+        $userId = $I->grabDataFromResponseByJsonPath("_id")[0];
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseContainsJson(['status' => 'ok']);
-        $I->sendGet('people', $userData);
+
+        $I->sendGet(API::$GET, $userData);
         $I->seeResponseMatchesJsonType($defaultSchema);
-    }
 
-    /**
-     * Проверка негативного сценария на создание пользователся без name
-     * @group test_negative
-     * @dataProvider getDataForCreateUserNegative
-     * @param Example $data
-     */
-    public function checkUserCreateNegative(\FunctionalTester $I, Example $data)
-    {
-        $faker = Factory::create();
+        $I->sendPut(API::$VARIOUS.$userId, ['job'=> $faker->company]);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseContainsJson($data['nModified']);
 
-        $email = $faker->email;
-        $owner = $faker->lastName . '@alex_radko';
-        $I->haveHttpHeader('Content-Type', 'application/json');
-        $I->sendPost('human', [
-            $data['email'] ? $email : null, 
-            $data['owner'] ? $owner : null,
-        ]);
-
-
+        $I->sendDelete(API::$VARIOUS.$userId);
+        $I->seeResponseContainsJson($data['deletedCount']);
+        $I->seeResponseCodeIsSuccessful();
+        $I->sendGet(API::$VARIOUS.$userId);
         $I->seeResponseContainsJson($data['errorText']);
     }
 
     /**
      * dataProvider для теста
      */
-    protected function getDataForCreateUserNegative()
+    protected function dataForcheckUserCreate()
     {
         return [
             [
-                'email' => true,
-                'owner' => false,
-                'errorText' => ['message' => 'email не передан']
-            ],
-            [
-                'email' => false,
-                'owner' => true,
-                'errorText' => ['message' => 'email не передан']
+                'nModified'    => ['nModified' => 1],
+                'deletedCount' => ['deletedCount' => 1],
+                'errorText'    => ['error' => 'Not Found']
             ]
         ];
     }
